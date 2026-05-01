8dfaa91907f571d25377fe08f5eee26910afb02e
Anton Dodonov <anton.v.dodonov@gmail.com>
2026-05-01 10:26:14 +0700

    web fix

--- app/bhagavadgita.book/lib/data/local/app_database.dart
+++ app/bhagavadgita.book/lib/data/local/app_database.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
-
 import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
-import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart';
 
 import 'tables.dart';
+import 'app_database_connection.dart'
+    if (dart.library.io) 'app_database_connection_io.dart'
+    if (dart.library.html) 'app_database_connection_web.dart';
 
 part 'app_database.g.dart';
 
@@ -22,17 +20,9 @@ part 'app_database.g.dart';
   ],
 )
 class AppDatabase extends _$AppDatabase {
-  AppDatabase() : super(_openConnection());
+  AppDatabase() : super(openConnection());
   AppDatabase.forTesting(super.executor);
 
   @override
   int get schemaVersion => 1;
-}
-
-LazyDatabase _openConnection() {
-  return LazyDatabase(() async {
-    final dir = await getApplicationDocumentsDirectory();
-    final file = p.join(dir.path, 'bhagavadgita.sqlite');
-    return NativeDatabase.createInBackground(File(file));
-  });
 }
